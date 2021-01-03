e7e22cb616bd578c10a9bb11be632b1c9fa070c5
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-01-03 16:00:58 +1100

    Added register functionality

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+
+      //Validate
+      $this->validate($request, [
+          'name'=> 'required',
+          'username'=> 'required',
+          'email'=> 'required|email',
+          'password'=> 'required|confirmed',
+      ]);
+
+      //Store the user into the database using the user model
+      User::create([
+          'name' => $request->name,
+          'username' => $request->username,
+          'email' => $request->email,
+          'password' => Hash::make($request->password),
+      ]);
+
+      //Sign the user in
+      /*Auth::attempt([
+          'email' => $request->email,
+          'password' => $request->password,
+          ]);
+          */
+
+      //redirect
+      return redirect()->route('dashboard');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,58 @@
 
 @section('content')
   <div class="container text center">
-      <p>Register</p>
+    <div class="card">
+        <div class="card-header text-center">
+          Register
+        </div>
+        <div class="card-body">
+            <form action="{{ route('register') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="name" placeholder="Name" aria-label="Name" aria-describedby="basic-addon1" value="{{old('name')}}">
+                </div>
+                @error('name')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" name="username" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value="{{old('username')}}">
+                </div>
+                @error('username')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" name="email" placeholder="Email" aria-label="Name" aria-describedby="basic-addon1" value="{{old('email')}}">
+                </div>
+                @error('email')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" name="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                </div>
+                @error('password')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" name="password_confirmation" placeholder="Confirm Password" aria-label="Confirm-Password" aria-describedby="basic-addon1">
+                </div>
+                @error('password_confirmation')
+                    <div class="alert alert-danger" role="alert">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <button class="btn btn-primary" type="submit">Register</button>
+                </div>
+            </form>
+        </div>
+      </div>
   </div>
 @endsection
