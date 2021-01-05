6f8e2d144e272bb1031122e1c9e8194d678048a3
Callum9362 <callum.gill.1993.cg@gmail.com>
2021-01-05 15:49:56 +1100

    Mapped more routes to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,10 +18,10 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="{{ route('home') }}">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Dashboard</a>
+                <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Posts</a>
